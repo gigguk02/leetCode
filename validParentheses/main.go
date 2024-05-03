90446faac6acb3bca639ef6b8fc2416766e2dfe5
Ваше Имя <you@example.com>
2024-05-03 16:54:49 +0500

    .

---
+++ validParentheses/main.go
@@ -0,0 +1,29 @@
+package main
+
+func isValid(s string) bool {
+	stack := make([]rune, 0)
+
+	for _, char := range s {
+		switch char {
+		case '(', '[', '{':
+			stack = append(stack, char)
+		case ')':
+			if len(stack) == 0 || stack[len(stack)-1] != '(' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case ']':
+			if len(stack) == 0 || stack[len(stack)-1] != '[' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case '}':
+			if len(stack) == 0 || stack[len(stack)-1] != '{' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
