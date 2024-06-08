5b30f5abb605ce5247b4a438ae85e182db1d2f55
Ваше Имя <you@example.com>
2024-06-08 16:14:58 +0500

    .

---
+++ searchInsert/removeElement/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(removeElement([]int{3, 2, 2, 3}, 3))
+
+}
+func removeElement(nums []int, val int) int {
+	k := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+	return k
+}
