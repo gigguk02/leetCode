a8c6fb0c414be8ba539a2e73affb5ad6a977b6f3
linkiog <g.bekbolatova02@mail.ru>
2024-06-09 19:39:31 +0500

    .

--- climbStairs/main.go
+++ climbStairs/main.go
@@ -7,11 +7,14 @@ func main() {
 }
 
 func climbStairs(n int) int {
-	if n == 0 {
+	if n == 0 || n == 1 {
 		return 1
 	}
-	if n == 1 {
-		return 1
+	dp := make([]int, n+1)
+	dp[0] = 1
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
 	}
-	return climbStairs(n-1) + climbStairs(n-2)
+	return dp[n]
 }
