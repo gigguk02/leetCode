28b678e62f66f69426fe9f96ea059d923a755c23
linkiog <g.bekbolatova02@mail.ru>
2024-06-09 19:35:58 +0500

    .

---
+++ climbStairs/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(climbStairs(4))
+}
+
+func climbStairs(n int) int {
+	if n == 0 {
+		return 1
+	}
+	if n == 1 {
+		return 1
+	}
+	return climbStairs(n-1) + climbStairs(n-2)
+}
