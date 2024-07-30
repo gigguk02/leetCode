19b548cdeb6815d5890628e6387890788eb37472
linkiog <g.bekbolatova02@mail.ru>
2024-07-30 21:23:14 +0500

    .

---
+++ symmetricTree/main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main() {
+	root := TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 3,
+			},
+			Right: &TreeNode{
+				Val: 4,
+			},
+		},
+		Right: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 4,
+			},
+			Right: &TreeNode{
+				Val: 3,
+			},
+		},
+	}
+	fmt.Println(isSymmetric(&root))
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	return isMirror(root, root)
+}
+
+func isMirror(node1 *TreeNode, node2 *TreeNode) bool {
+	if node1 == nil && node2 == nil {
+		return true
+	}
+
+	if node1 == nil || node2 == nil {
+		return false
+	}
+
+	return (node1.Val == node2.Val) && isMirror(node1.Left, node2.Right) && isMirror(node1.Right, node2.Left)
+}
