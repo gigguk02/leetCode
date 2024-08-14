b176e778beff4ca0278d13398e2e357b45369484
linkiog <g.bekbolatova02@mail.ru>
2024-08-15 00:51:07 +0500

    .

---
+++ bestTimetoBuyandSellStock/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+	fmt.Println(maxProfit([]int{7, 6, 4, 3, 1}))
+	fmt.Println(maxProfit([]int{2, 4, 1}))
+}
+
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+	minPrice := prices[0]
+	maxProfit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		} else {
+			profit := prices[i] - minPrice
+			if profit > maxProfit {
+				maxProfit = profit
+			}
+		}
+	}
+
+	return maxProfit
+}
